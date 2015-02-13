f58d6becc2551ebbfe0773ff2fb66de48c892a84
jixingyu <ymx2011@gmail.com>
2015-02-13 17:02:07 +0800

    update

--- source/game/views/turntable.php
+++ source/game/views/turntable.php
@@ -1,19 +1,10 @@
 <?php include('_header.php');?>
-<style type="text/css">
-#desc {
-	display: none;
-	position: absolute;
-	color: #C0572D;
-	overflow-y: auto;
-}
-</style>
 <script type="text/javascript">
 var isLogin = <?php echo $isLogin;?>;
 var turntableImage = '<?php echo $image;?>';
 var freeNum = <?php echo $freeNum;?>;
 var todayNum = <?php echo $todayNum;?>;
 var consumePoints = <?php echo $consumePoints;?>;
-var nickname = '<?php echo $nickname ?: "匿名";?>';
 var userpoints = <?php echo $points;?>;
 var desc = "<?php echo $desc;?>";
 </script>
